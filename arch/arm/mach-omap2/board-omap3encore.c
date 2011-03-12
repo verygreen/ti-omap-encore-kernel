14c3666adac634c300071a43916e29db9b90c53c
Oleg Drokin <green@linuxhacker.ru>
2011-03-11 21:10:40 -0500

    Make USB to actually work. Drop android bits from board file as no longer required - generic omap usb code takes care of that now.

--- arch/arm/mach-omap2/board-omap3encore.c
+++ arch/arm/mach-omap2/board-omap3encore.c
@@ -488,7 +488,7 @@ static struct i2c_board_info __initdata encore_i2c_bus2_info[] = {
 
 static struct omap_musb_board_data musb_board_data = {
 	.interface_type		= MUSB_INTERFACE_ULPI,
-	.mode			= MUSB_OTG,
+	.mode			= MUSB_PERIPHERAL,
 	.power			= 100,
 };
 
@@ -529,97 +529,6 @@ static struct omap_uart_port_info omap_serial_platform_data[] = {
 		.flags		= 0
 	}
 };
-
-#if defined(CONFIG_USB_ANDROID) || defined(CONFIG_USB_ANDROID_MODULE)
-static struct usb_mass_storage_platform_data mass_storage_pdata = {
-	.vendor = "B&N     ",
-	.product = "Ebook Disk      ",
-	.release = 0x0100,
-};
-
-static struct platform_device usb_mass_storage_device = {
-	.name = "usb_mass_storage",
-	.id = -1,
-	.dev = {
-		.platform_data = &mass_storage_pdata,
-		},
-};
-
-// Reserved for serial number passed in from the bootloader.
-static char adb_serial_number[32] = "";
-
-static char *usb_functions_ums[] = {
-	"usb_mass_storage",
-};
-
-static char *usb_functions_ums_adb[] = {
-	"usb_mass_storage",
-	"adb",
-};
-
-static char *usb_functions_rndis[] = {
-	"rndis",
-};
-
-static char *usb_functions_rndis_adb[] = {
-	"rndis",
-	"adb",
-};
-
-static char *usb_functions_all[] = {
-#ifdef CONFIG_USB_ANDROID_RNDIS
-	"rndis",
-#endif
-	"usb_mass_storage",
-	"adb",
-#ifdef CONFIG_USB_ANDROID_ACM
-	"acm",
-#endif
-};
-
-static struct android_usb_product usb_products[] = {
-	{
-		.product_id	= ENCORE_PRODUCT_ID,
-		.num_functions	= ARRAY_SIZE(usb_functions_ums),
-		.functions	= usb_functions_ums,
-	},
-	{
-		.product_id	= ENCORE_ADB_PRODUCT_ID,
-		.num_functions	= ARRAY_SIZE(usb_functions_ums_adb),
-		.functions	= usb_functions_ums_adb,
-	},
-	{
-		.product_id	= ENCORE_RNDIS_PRODUCT_ID,
-		.num_functions	= ARRAY_SIZE(usb_functions_rndis),
-		.functions	= usb_functions_rndis,
-	},
-	{
-		.product_id	= ENCORE_RNDIS_ADB_PRODUCT_ID,
-		.num_functions	= ARRAY_SIZE(usb_functions_rndis_adb),
-		.functions	= usb_functions_rndis_adb,
-	},
-};
-
-static struct android_usb_platform_data android_usb_pdata = {
-	.vendor_id	= ENCORE_VENDOR_ID,
-	.product_id	= ENCORE_PRODUCT_ID,
-	.manufacturer_name = "B&N",
-	.product_name	= "NookColor",
-	.serial_number	= "11223344556677",
-	.num_products   = ARRAY_SIZE(usb_products),
-	.products	= usb_products,
-	.num_functions	= ARRAY_SIZE(usb_functions_all),
-	.functions	= usb_functions_all,
-};
-
-static struct platform_device android_usb_device = {
-	.name		= "android_usb",
-	.id		= -1,
-	.dev		= {
-		.platform_data = &android_usb_pdata,
-	},
-};
-#endif
 
 #ifdef CONFIG_OMAP_MUX
 static struct omap_board_mux board_mux[] __initdata = {
@@ -1004,17 +913,6 @@ static void __init omap_encore_init(void)
 #endif
 #endif
 
-
-#if defined(CONFIG_USB_ANDROID) || defined(CONFIG_USB_ANDROID_MODULE)
-	platform_device_register(&usb_mass_storage_device);
-	// Set the device serial number passed in from the bootloader.
-	if (system_serial_high != 0 || system_serial_low != 0) {
-		snprintf(adb_serial_number, sizeof(adb_serial_number), "%08x%08x", system_serial_high, system_serial_low);
-		adb_serial_number[16] = '\0';
-		android_usb_pdata.serial_number = adb_serial_number;
-	}
-	platform_device_register(&android_usb_device);
-#endif
         BUG_ON(!cpu_is_omap3630());
 }
 
